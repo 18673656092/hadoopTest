894712ec1a76bd571c6b768fc1ce116713cd97ca
Administrator <admin@example.com>
2018-08-21 18:16:42 +0800

    block

--- src/test/java/TestBlock.java
+++ src/test/java/TestBlock.java
@@ -8,8 +8,9 @@ public class TestBlock {
     @Test
     public void Test() {
         Block block = Block.getInstace();
-        block.addBlock("zhuran","zhuran");
-        block.addBlock("laoda","6d59c1a54470a96d3665fb2ba1bd8ae8f7c51e2823ec6345660bf605742e0a3f");
+        block.addBlock("send 1 BTC to C","zhuran");
+        block.addBlock("send 2 BTC to D","zhuran");
+        block.addBlock("send 1 BTC to B","zhuran");
         block.say();
     }
 }
